8a4c693071c6746b20275ba327d73b3a6328d577
Anthony Pagone <apagone@uci.edu>
2026-01-28 18:06:44 -0800

    Added ESP32 Arduino PWM
    Signed-off-by: Anthony Pagone <apagone@uci.edu>

---
+++ motor_controllers/esp32_arduino/pwm/pwm.ino
@@ -0,0 +1,24 @@
+// See this link for the LEDC documentation:
+// https://docs.espressif.com/projects/arduino-esp32/en/latest/api/ledc.html
+
+uint8_t PWM_PIN = 23;
+uint32_t FREQUENCY = 50;
+// 5% of 255 = 12.75 = 12
+uint32_t STOP_DUTY = 12;
+
+void setup() {
+  // Initialize serial communication at 115200 bits per second:
+  Serial.begin(115200);
+
+  // Setup timer with given frequency, resolution and attach it to a led pin with auto-selected channel
+  ledcAttach(PWM_PIN, FREQUENCY, LEDC_TIMER_12_BIT);
+  // Set duty cycle
+  ledcWrite(PWM_PIN, STOP_DUTY);
+  Serial.print("Current duty: ");
+  Serial.println(ledcRead(PWM_PIN));
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
