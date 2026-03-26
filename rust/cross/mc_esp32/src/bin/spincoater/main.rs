2913b5fe480a28c5be12bf35519bbdec50894d43
Anthony Pagone <anthony21@pagone.com>
2026-03-25 17:09:49 -0700

    Add clarification for environment variable compile errors
    Using option_env is a bad idea here because if you forget to set the
    environment variable, you’ll have to reflash, which wastes a lot of
    time.

--- rust/cross/mc_esp32/src/bin/spincoater/main.rs
+++ rust/cross/mc_esp32/src/bin/spincoater/main.rs
@@ -74,9 +74,15 @@ extern crate alloc;
 // Do not hardcode sensitive information like this.
 // Instead, pass in the variables as environment variables when you compile, like this:
 // SSID=_ PASSWORD=_ cargo run --release
-const SSID: &str = env!("SSID");
+const SSID: &str = env!(
+    "SSID",
+    "If you see this error in the editor, ignore it. Otherwise, set the environment variable."
+);
 /// Note: Password must be 8-64 characters.
-const PASSWORD: &str = env!("PASSWORD");
+const PASSWORD: &str = env!(
+    "PASSWORD",
+    "if you see this error in the editor, ignore it. Otherwise, set the environment variable"
+);
 
 // This creates a default app-descriptor required by the esp-idf bootloader.
 // For more information see: <https://docs.espressif.com/projects/esp-idf/en/stable/esp32/api-reference/system/app_image_format.html#application-description>
