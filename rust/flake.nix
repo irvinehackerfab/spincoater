fa7435531b856029e3ff41bef4ae0fa99787cc09
KingRocco21 <anthony21@pagone.com>
2026-05-03 00:35:53 -0700

    Add dev environment for host PC programs

---
+++ rust/flake.nix
@@ -0,0 +1,55 @@
+{
+  description = "A dev shell for Rust development";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      rust-overlay,
+      flake-utils,
+      ...
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
+        custom-rust-bin = pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
+      in
+      {
+        devShells.default =
+          with pkgs;
+          mkShell {
+            strictDeps = true;
+            # host/target agnostic programs
+            depsBuildBuild = [
+            ];
+            # compilers & linkers & dependency finding programs
+            nativeBuildInputs = [
+              custom-rust-bin
+              pkg-config
+              eza
+              fd
+            ];
+            # libraries
+            buildInputs = [
+              openssl
+              wayland
+            ];
+
+            shellHook = ''
+              alias ls=eza
+              alias find=fd
+            '';
+          };
+      }
+    );
+}
